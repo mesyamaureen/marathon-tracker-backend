6c28df598452e9a8bb52d494f5bd8beec6a3a706
mesyamaureen <mesyamaureen@gmail.com>
2024-04-12 12:32:33 +0200

    spring boot app initial commit

---
+++ marathon-tracker/src/main/java/marathontracker/htwwebtech/MarathonTrackerApplication.java
@@ -0,0 +1,13 @@
+package marathontracker.htwwebtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarathonTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarathonTrackerApplication.class, args);
+	}
+
+}
---
+++ marathon-tracker/src/test/java/marathontracker/htwwebtech/MarathonTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package marathontracker.htwwebtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarathonTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
