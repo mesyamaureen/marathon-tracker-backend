1b0fe01b4c68c524c5aee5010cc58b69cc78e2d6
mesyamaureen <mesyamaureen@gmail.com>
2024-04-23 12:45:35 +0200

    jdbc driver to postgresql changed and hello world fallback added

---
+++ marathon-tracker/src/main/java/marathontracker/htwwebtech/HelloWorld.java
@@ -0,0 +1,12 @@
+package marathontracker.htwwebtech;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
