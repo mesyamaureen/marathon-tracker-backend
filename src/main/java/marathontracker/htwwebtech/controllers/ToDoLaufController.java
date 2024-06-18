429700ce0bb68d1d6a9e2992e96e835977778dba
mesyamaureen <mesyamaureen@gmail.com>
2024-06-18 14:51:13 +0200

    cors for todolauf added

--- src/main/java/marathontracker/htwwebtech/controllers/ToDoLaufController.java
+++ src/main/java/marathontracker/htwwebtech/controllers/ToDoLaufController.java
@@ -12,27 +12,32 @@ public class ToDoLaufController {
     @Autowired
     ToDoLaufService service;
 
+    @CrossOrigin(origins = "https://marathon-tracker-frontend.onrender.com")
     @PostMapping("/todolauf")
     public ToDoLauf createToDoLauf(@RequestBody ToDoLauf toDoLauf) {
         return service.save(toDoLauf);
     }
 
+    @CrossOrigin(origins = "https://marathon-tracker-frontend.onrender.com")
     @GetMapping("/alleToDoLaufs/{id}")
     public ToDoLauf getToDoLauf(@PathVariable String id) {
         Long toDoLaufId = Long.parseLong(id);
         return service.get(toDoLaufId);
     }
 
+    @CrossOrigin(origins = "https://marathon-tracker-frontend.onrender.com")
     @GetMapping("/alleToDoLaufs")
     public List<ToDoLauf> getToDoLaufs() {
         return service.getToDoLaufs();
     }
 
+    @CrossOrigin(origins = "https://marathon-tracker-frontend.onrender.com")
     @PutMapping("/alleToDoLaufs/{id}")
     public ToDoLauf updateToDoLauf(@PathVariable Long id, @RequestBody ToDoLauf toDoLauf) {
         return service.update(id, toDoLauf);
     }
 
+    @CrossOrigin(origins = "https://marathon-tracker-frontend.onrender.com")
     @DeleteMapping("/alleToDoLaufs/{id}")
     public boolean deleteToDoLauf(@PathVariable String id) {
         Long toDoLaufId = Long.parseLong(id);
