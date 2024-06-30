e295a88cfd6cc9b46de80466704db1d434e7db4c
mesyamaureen <mesyamaureen@gmail.com>
2024-06-30 13:24:50 +0200

    dependency to junit assert j for testing added

---
+++ src/test/java/marathontracker/htwwebtech/entities/LaufTest.java
@@ -0,0 +1,21 @@
+package marathontracker.htwwebtech.entities;
+
+import org.junit.jupiter.api.Test;
+
+public class LaufTest {
+    // TODO: Implement Unit tests
+
+    // Test if the constructor works correctly
+    @Test
+    public void testConstructor() {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    // Test if the getter and setter methods work correctly
+
+    // Test if the toString method works correctly
+
+    // Test if the equals method works correctly
+}
